ed4fe43dd741002f44bc5d0969f11e62f1f90ed2
Donald Westland <don@MacBook-Pro-1T.local>
2021-07-24 13:04:43 -0700

    Add SASS

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/normalize.scss'
+import '../styles/base.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
