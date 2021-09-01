ecdf061bc9081be725ae922e88d7a15855195945
dwestland <don@westland.net>
2021-09-01 07:26:34 -0700

    Add CSS to navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -6,9 +6,12 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <ul>
-        <li>
+        <li className="dropdown dropdown-6">
+          <Link href="/"><a>Home</a></Link>
+        </li>
+        <li className="dropdown dropdown-6">
           Hawaii Beach Cams
-          <ul>
+          <ul className="dropdown_menu dropdown_menu--animated dropdown_menu-6">
             <li><Link href="/hawaii/"><a>Hawaii</a></Link></li>
             <li><Link href="/hawaii/kauai/"><a>Kauai</a></Link></li>
             <li><Link href="/hawaii/oahu/"><a>Oahu</a></Link></li>
@@ -17,9 +20,9 @@ export default function Navbar() {
           </ul>
 
         </li>
-        <li>
+        <li className="dropdown dropdown-6">
           California Beach Cams
-          <ul>
+          <ul className="dropdown_menu dropdown_menu--animated dropdown_menu-6">
             <li><Link href="/california/"><a>California</a></Link></li>
             <li><Link href="/california/san-diego/"><a>San Diego</a></Link></li>
             <li><Link href="/california/los-angeles/"><a>Los Angeles</a></Link></li>
@@ -27,9 +30,9 @@ export default function Navbar() {
             <li><Link href="/california/san-francisco/"><a>San Francisco</a></Link></li>
           </ul>
         </li>
-        <li>
+        <li className="dropdown dropdown-6">
           Florida Beach Cams
-          <ul>
+          <ul className="dropdown_menu dropdown_menu--animated dropdown_menu-6">
             <li><Link href="/florida/"><a>Florida</a></Link></li>
             <li><Link href="/florida/panhandle/"><a>Pan Handel</a></Link></li>
             <li><Link href="/florida/northeast/"><a>Northeast</a></Link></li>
