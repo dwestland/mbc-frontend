3f0f6f3f15e1082f6018f57c87e7188e312e3c6a
dwestland <don@westland.net>
2021-09-06 19:48:02 -0700

    Update showcase

--- components/Showcase.js
+++ components/Showcase.js
@@ -3,8 +3,14 @@ import styles from '@/styles/Showcase.module.scss'
 export default function Showcase() {
   return (
     <div className={styles.showcase}>
-      <h2 className="heading">MyBeachCams.com</h2>
-      <h3 className="subHeading">Live Webcams from Hawaii, Florida and California</h3>
+      <div className={styles.container}>
+      <span className={styles.logo}>
+        My Beach Cams.com
+      </span>
+        <h3 className={styles.subHeading}>
+          Live Webcams from Hawaii, Florida and California
+        </h3>
+      </div>
     </div>
   )
 }
