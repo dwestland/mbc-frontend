2798837a0e6278e6c8f585d05eedd1a66a83b409
dwestland <don@westland.net>
2021-09-21 19:53:03 -0700

    Add Logout page

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -63,15 +63,14 @@ export const AuthProvider = ({ children }) => {
 
   // Logout user
   const logout = async () => {
-    console.log('%c Logout ', 'background: red; color: white', )
-    // const res = await fetch(`${NEXT_URL}/api/logout`, {
-    //   method: 'POST',
-    // })
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST',
+    })
 
-    // if (res.ok) {
-    //   setUser(null)
-    //   router.push('/')
-    // }
+    if (res.ok) {
+      setUser(null)
+      router.push('/')
+    }
   }
 
   // Check if user is logged in
